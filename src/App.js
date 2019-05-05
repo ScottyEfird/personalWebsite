7e5389c3f396da26fa8b7222b749c835867974d5
Scott Efird <efirds@hotmail.com>
2019-05-05 08:04:09 -0600

    a lot of basic stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import './App.css'
 
-import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react'
-
-import cloudinary from 'cloudinary-core'
-
-// const cloudinaryCore = new cloudinary.Cloudinary({cloud_name: 'demo'})
-const SampleImg = () => (
-  <img src='https://res.cloudinary.com/demo/image/upload/sample.jpg' />
-)
-
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-      dogs
-        {SampleImg}
+        <img src={'./images/Gels-7.jpg'} />
       </header>
     </div>
   )
