c04d364faf6285e4e94a5d76e82934b23b46ce8e
Scott Efird <efirds@hotmail.com>
2019-05-05 17:47:18 -0600

    Updated CSS and default func

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # Stuff and things
+## Installing /Debugging
+Node, NPM and Yarn are required installs.
+ * Navagate to the project repo
+ * `yarn` To install dependencies
+ * `yarn start` to deploy locally to `http://localhost:3000`
+
 ### Building and Deploying
  * `yarn build`
- * TODO Write script to move `now.json` into `/build` dir
- * Navagate to the build directory and use `now --target production` to deploy. 
+ * `cp .\now.json .\build\`
+ * `cd .\build\`
+ * Use `now --target production` to deploy. 
 
 ### Info
  * Hosted using Zeit via now. `zeit.co`
  
 ### Domain Name
  * Use `now domains ls` to check domain for usable domain names.
-
  * You might need to `move` the domain to your GitHub account. 
-
  * `LostInOtter.space`
-
  * `now alias <personalwebsite-xyz123.now.sh> lostinotter.space`
--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 import './App.css'
 
+const TOTAL_PHOTOS = 75
+
 function Index() {
   let photos = []
-  for (let i = 1; i <= 75; i++) {
+  for (let i = 1; i <= TOTAL_PHOTOS; i++) {
     photos.push(
       <Link to={`/photos/${i}`} key={i} className='FlexItem' >
-        <img src={`./images/thumbnails/tako (${i})_tn.jpg`} height={300} width={300} alt={''} />
+        <img src={`./images/thumbnails/tako${i}_tn.jpg`} height={300} width={300} alt={''} />
       </Link>
     )
   }
   return (
-
     <div className='FlexBody'>
       {photos}
     </div>
@@ -22,18 +23,49 @@ function Index() {
 
 function Photos({ match }) {
   const { params: id } = match
+
   return (
     <div className='App'>
-      <img src={`${window.location.origin}/images/photos/tako (${id.id}).jpg`} alt={''} />
+      <img src={`${window.location.origin}/images/photos/tako${id.id}.jpg`} alt={''} className='MaxWidth' />
     </div>
   )
+}
+
+const getRandomNumber = () => Math.floor(Math.random() * (TOTAL_PHOTOS - 1 + 1)) + 1
+
+class Photo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      urlNumber: getRandomNumber()
+    }
+  }
+
+  handleClick () {
+    this.setState({
+      urlNumber: getRandomNumber()
+    })
+  }
+
+  render () {
+    return (
+      <div className='App' onClick={() => this.handleClick()} >
+        <img
+          src={`${window.location.origin}/images/photos/tako${this.state.urlNumber}.jpg`}
+          alt={''}
+          className='MaxWidth'
+        />
+      </div>
+    )
+  }
 }
 
 function AppRouter() {
   return (
     <Router>
       <div className='App'>
-        <Route path='/' exact component={Index} />
+        <Route path='/' exact component={Photo} />
+        <Route path='/photos' component={Photo} />
         <Route path='/photos/:id' component={Photos} />
       </div>
     </Router>
