15bba38c0842c6faab6456a20e073ff685f76127
Scott Efird <efirds@hotmail.com>
2019-05-04 19:37:19 -0600

    INIT

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react'
+
+import cloudinary from 'cloudinary-core'
+
+// const cloudinaryCore = new cloudinary.Cloudinary({cloud_name: 'demo'})
+const SampleImg = () => (
+  <img src='https://res.cloudinary.com/demo/image/upload/sample.jpg' />
+)
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+      dogs
+        {SampleImg}
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
