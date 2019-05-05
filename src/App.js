21916fcf6558f1bbe061d8be6a23f4fe739ecf7a
Scott Efird <efirds@hotmail.com>
2019-05-05 14:14:06 -0600

    Basic workflow created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
 import './App.css'
 
-function App() {
+function Index() {
+  let photos = []
+  for (let i = 0; i <= 4; i++) {
+    photos.push(
+      <Link to={`/photos/${i}`} key={i} className='FlexItem' target='_blank' >
+        <img src={`./thumbnails/${i}_tn.jpg`} height={300} width={300} alt={''} />
+      </Link>
+    )
+  }
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={'./images/Gels-7.jpg'} />
-      </header>
+
+    <div className='FlexBody'>
+      {photos}
     </div>
   )
 }
 
-export default App
+function Photos({ match }) {
+  const { params: id } = match
+  console.log(id.id)
+  return (
+    <div className='App'>
+      <img src={`../images/${id.id}.jpg`} alt={''} />
+    </div>
+  )
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div className='App'>
+        <Route path='/' exact component={Index} />
+        <Route path='/photos/:id' component={Photos} />
+      </div>
+    </Router>
+  )
+}
+
+export default AppRouter
