e65ee7ae0666b80876c72dd9eb1da967d297f0b5
Scotty <scottyefird@gmail.com>
2020-08-20 12:48:37 -0400

    Updated the old version of the site

--- src/Master/Utils/Experience.jsx
+++ src/Master/Utils/Experience.jsx
@@ -1,50 +1,66 @@
-import { JobKeys } from './utilities.jsx'
+import { JobKeys } from "./utilities.jsx";
+
+export const FullSail = {
+  title: "Full Sail University",
+  key: JobKeys.FULLSAIL,
+  jobTitle: "Front End Software Engineer",
+  location: "Orlando, FL",
+  dateToFrom: "November 2019 - September 2020",
+  logo: "fullsail.png",
+  url: "https://www.fullsail.edu/",
+  experianceData: [
+    "Contributed valuable front end code that impacted tens of thousands of users",
+    "Worked with a with a highly experienced team to provide updates and improvements to the user experience",
+    "Strong focus on mobile first design and creating layouts that worked at any screen size",
+    "Assisted in a major version change involving refactoring thousands of lines of code",
+  ],
+};
 
 export const RiskSense = {
-  title: 'RiskSense',
+  title: "RiskSense",
   key: JobKeys.RISKSENSE,
-  jobTitle: 'Front End Software Engineer',
-  location: 'Albuquerque, NM',
-  dateToFrom: 'June 2018 - Current',
-  logo: 'risksense.png',
-  url: 'https://risksense.com/',
+  jobTitle: "Front End Software Engineer",
+  location: "Albuquerque, NM",
+  dateToFrom: "June 2018 - November 2019",
+  logo: "risksense.png",
+  url: "https://risksense.com/",
   experianceData: [
-    'Embedded front end developer, primarily created integrations and dashboards',
-    'Responsible for the planning, architecting and delivering of software',
-    'Refactored several large legacy systems to a modern ES6 architecture',
-    'Helped lead the onboarding and training of an offshore front end development team',
-    'Predominantly worked with large data sets containing 100k+ entities per table',
-  ]
-}
+    "Embedded front end developer creating configurable integrations and dashboards",
+    "Helped lead the onboarding and training of an offshore front end development team",
+    "Predominately worked with large data sets containing 100k+ entities per table",
+    "Strong teamwork and communication skills used to meet highly technical specifications",
+    "Responsible for the planning, architecting and delivering of software",
+  ],
+};
 
 export const Coolfire = {
-  title: 'Coolfire',
+  title: "Coolfire",
   key: JobKeys.COOLFIRE,
-  jobTitle: 'Front End Software Engineer',
-  location: 'St. Louis, MO',
-  dateToFrom: 'July 2016 - June 2018',
-  logo: 'coolfire.png',
-  url: 'https://www.coolfiresolutions.com/',
+  jobTitle: "Front End Software Engineer",
+  location: "St. Louis, MO",
+  dateToFrom: "July 2016 - June 2018",
+  logo: "coolfire.png",
+  url: "https://www.coolfiresolutions.com/",
   experianceData: [
-    'Worked on realtime dashboards, Leaflet maps and instant messaging apps',
-    'Developed in the MERN stack (MongoDB, ExpressJs, NodeJs, Babel and Webpack)',
-    'Helped create a real time inventory and personnel tracking webapp',
-    'Handled and utilized JSON data from RESTful and Socket.IO endpoints',
-    'Utilized popular packages including: React-Router, React-Redux, Styled-Components and Redux-Saga',
-  ]
-}
+    "Worked in creating dynamically updating user interfaces that displayed real time data",
+    "Utilized Socket.IO data streams to provide instant updates to the web app",
+    "Helped create a real time inventory and personnel tracking webapp",
+    "Developed in the MERN stack (MongoDB, ExpressJs, NodeJs, Babel and Webpack)",
+    "Utilized popular packages including: React-Router, React-Redux, Styled-Components and Redux-Saga",
+  ],
+};
 
 export const Frii = {
-  title: 'Frii',
+  title: "Frii",
   key: JobKeys.FRII,
-  jobTitle: 'NOC Engineer - Intern',
-  location: 'Fort Collins, CO',
-  dateToFrom: 'Summer of 2015',
-  logo: 'frii.png',
-  url: 'https://frii.com/',
+  jobTitle: "NOC Engineer - Intern",
+  location: "Fort Collins, CO",
+  dateToFrom: "Summer of 2015",
+  logo: "frii.png",
+  url: "https://frii.com/",
   experianceData: [
-    'Maintained and monitored the stability of an ISP fiber network in northern Colorado',
-    'Solved tickets involving everything from web server maintenance to installing data center hardware',
-    'Worked with a wide range of hardware including Cambium Networks, Ruckus and Ubiquiti APs',
-  ]
-}
+    "Maintained and monitored the stability of an ISP fiber network in northern Colorado",
+    "Solved tickets involving everything from web server maintenance to installing data center hardware",
+    "Worked with a wide range of hardware including Cambium Networks, Ruckus and Ubiquiti APs",
+  ],
+};
--- src/Master/Utils/utilities.jsx
+++ src/Master/Utils/utilities.jsx
@@ -1,9 +1,12 @@
 export const JobKeys = {
   GITHUB: "github",
   RISKSENSE: "risksense",
-  FRII: 'frii',
-  COOLFIRE: 'coolfire'
-}
+  FRII: "frii",
+  COOLFIRE: "coolfire",
+  FULLSAIL: "fullsail",
+};
 
 // TODO Tests
-export const isButtonOpen = (buttons, key) => { return buttons.find(x => x.key === key).isOpen }
+export const isButtonOpen = (buttons, key) => {
+  return buttons.find((x) => x.key === key).isOpen;
+};
