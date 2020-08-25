6e3955472d9c7f012ac9b3d02651464e8c3035bc
Scotty <scottyefird@gmail.com>
2020-08-25 13:20:18 -0400

    Added transitions

--- src/Master/Components/ExperienceCard/ExperienceCard.jsx
+++ src/Master/Components/ExperienceCard/ExperienceCard.jsx
@@ -2,28 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import styles from "./experienceCard.module.scss";
-import globalStyles from "../global.module.scss";
 
-const ExperienceCard = ({ toggleModal, brand, title, dateToFrom, url }) => (
-  <div className={styles.cardWrapper}>
+const ExperienceCard = ({ toggleModal, brand, title, dateToFrom }) => (
+  <div className={styles.cardWrapper} onClick={() => toggleModal(brand)}>
     <div className={styles.cardImageWrapper}>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <img
-          src={`${window.location.origin}/images/${brand}.png`}
-          height={150}
-          alt={`Logo for ${title}`}
-        />
-      </a>
+      <img
+        src={`${window.location.origin}/images/${brand}.png`}
+        alt={`Logo for ${title}`}
+      />
     </div>
     <div className={styles.cardContent}>
       <div className={styles.cardTitle}>{title}</div>
       <div className={styles.cardDate}>{dateToFrom}</div>
-      <button
-        className={globalStyles.button}
-        onClick={() => toggleModal(brand)}
-      >
-        More info
-      </button>
     </div>
   </div>
 );
