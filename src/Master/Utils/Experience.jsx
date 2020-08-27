f048286813231e4e64e209afe9ff40c21cfed1cf
Scotty <scottyefird@gmail.com>
2020-08-27 10:13:32 -0400

    Updated experience

--- src/Master/Utils/Experience.jsx
+++ src/Master/Utils/Experience.jsx
@@ -7,7 +7,7 @@ const FULLSAIL = {
   url: "https://www.fullsail.edu/",
   project: "Full Sail Online",
   experience:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "While at Full Sail I helped implement valuable features that impacted tens of thousands of users. We put major emphasis on mobile first design and creating dynamic layouts that worked at any size. Working in React and alongside a wonderful highly experienced team I helped create a suite of auditing tools, reskinned components for a major version update and triage bugs.",
 };
 
 const RISKSENSE = {
@@ -19,7 +19,7 @@ const RISKSENSE = {
   url: "https://risksense.com/",
   project: "RiskSense Smart Integration Framework",
   experience:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Working as an embedded front end developer I created a dynamically configurable interface that allowed users to create and set up third party integrations with our web app. With a major focus on communication and teamwork, I helped in the onboarding with an offshore front end development team. Building high impact dashboards that pulled in hundreds of thousands of entities was a fun and exciting challenge that ended up being a valuable feature for the customers.",
 };
 
 const COOLFIRE = {
@@ -31,7 +31,7 @@ const COOLFIRE = {
   url: "https://www.coolfiresolutions.com/",
   project: "Coolfire Core",
   experience:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Working at Coofire inundated me with excited development problems that led to valuable features being created for the customers. We utilized real time socket IO data, Leaflet mapping and React to provide a fully dynamic dashboard that showed the locations and status of users in real time. I also participated in creating a standalone web app that allowed customers to automated workflows.",
 };
 
 const FRII = {
@@ -43,7 +43,7 @@ const FRII = {
   url: "https://frii.com/",
   project: "Network Operations and Datacenter Colocation",
   experience:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "My internship at FRII exposed me to a wide range of technology and helped teach vital skills. Working on tickets that ranged from physical hardware installation onsite to writing simple bash scripts that automate common tasks. Working with customers taught me valuable communication and problem solving skills.",
 };
 
 const EXPERIENCE = [FULLSAIL, RISKSENSE, COOLFIRE, FRII];
